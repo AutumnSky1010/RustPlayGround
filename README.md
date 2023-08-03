3986f2af8e47b867439537619b8a9817ee67e248
= <ranranruyuuto13@gmail.com>
2023-08-03 23:04:13 +0900

    Initial

---
+++ README.md
@@ -0,0 +1,2 @@
+# RustPlayGround
+Rustを使ったAPIサーバを題材に、慣れるためのリポジトリです。
---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, http::header};
+use actix_cors::Cors;
+
+#[get("/")]
+async fn root() -> impl Responder {
+    HttpResponse::Ok().body("hello")
+}
+
+#[actix_web::main]
+async fn main() -> Result<(), std::io::Error> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .wrap(
+                Cors::default()
+                    .allowed_origin("https://localhost:3000")
+                    .allowed_methods(vec!["GET", "POST"])
+                    .allowed_headers(vec![header::CONTENT_TYPE, header::CONTENT_TYPE])
+            )
+            .service(root)
+    });
+    let result = server.bind(("127.0.0.1", 8080))?.run().await;
+    return result;
+}
